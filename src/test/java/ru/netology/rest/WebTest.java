3e017ae0b26d1f1a22dffc9b2a6ed5a56202b00c
Ulugbek2080 <ulugbekabduzamilov@gmail.com>
2026-08-17 18:09:07 +0500

    Super final

--- src/test/java/ru/netology/rest/WebTest.java
+++ src/test/java/ru/netology/rest/WebTest.java
@@ -1,19 +1,12 @@
 package ru.netology.rest;
 import com.codeborne.selenide.SelenideElement;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class WebTest {
-    @BeforeAll
-    public static void setupAll() {
-        WebDriverManager.chromedriver().setup();
-    }
     @Test
     void rightInput() {
         open("http://localhost:9999");
@@ -22,6 +15,7 @@ public class WebTest {
         form.$("[data-test-id=phone] input").setValue("+76767676767");
         form.$("[data-test-id=agreement]").shouldBe(visible).click();
         form.$(".button").click();
-        $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
-    }
+        $("[data-test-id=order-success]")
+                .shouldBe(visible)
+                .shouldHave(text("Ваша заявка успешно отправлена!"));    }
 }
