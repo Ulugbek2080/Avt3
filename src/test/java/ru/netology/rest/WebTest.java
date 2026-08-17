7c765d9c87abde19984b174d6a2a6eb8270db78b
Ulugbek2080 <ulugbekabduzamilov@gmail.com>
2026-08-17 18:01:21 +0500

    back to 21

--- src/test/java/ru/netology/rest/WebTest.java
+++ src/test/java/ru/netology/rest/WebTest.java
@@ -1,5 +1,7 @@
 package ru.netology.rest;
 import com.codeborne.selenide.SelenideElement;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
@@ -8,6 +10,10 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class WebTest {
+    @BeforeAll
+    public static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
     @Test
     void rightInput() {
         open("http://localhost:9999");
