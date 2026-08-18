75f83cc65422d0ab8f1447cfa843b8a7aa5438b3
Ulugbek2080 <ulugbekabduzamilov@gmail.com>
2026-08-18 10:36:08 +0500

    Super puper final, поле восприминемает ё как русскую букву

--- src/test/java/ru/netology/rest/WebTest.java
+++ src/test/java/ru/netology/rest/WebTest.java
@@ -1,38 +1,40 @@
 package ru.netology.rest;
+
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class WebTest {
-    private SelenideElement form;
 
     @BeforeEach
     void openPage() {
         open("http://localhost:9999");
-        form = $(".form");
     }
 
     private void fillAndSubmit(String name, String phone, boolean agree) {
-        form.$("[data-test-id=name] input").setValue(name);
-        form.$("[data-test-id=phone] input").setValue(phone);
+        $("[data-test-id=name] input").setValue(name);
+        $("[data-test-id=phone] input").setValue(phone);
         if (agree) {
-            form.$("[data-test-id=agreement]").click();
+            $("[data-test-id=agreement]").click();
         }
-        form.$(".button").click();
+        $$("button").findBy(exactText("Продолжить")).click();
     }
 
     private SelenideElement error(String field) {
-        return form.$("[data-test-id=" + field + "].input_invalid .input__sub");
+        return $("[data-test-id=" + field + "].input_invalid .input__sub");
     }
 
     @Test
     void shouldSubmitValidForm() {
-        fillAndSubmit("Пётр Иванов-Сидоров", "+79012345678", true);
-        $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        fillAndSubmit("Русское Имя", "+79012345678", true);
+        $("[data-test-id=order-success]")
+                .shouldBe(visible)
+                .shouldHave(text("Ваша заявка успешно отправлена!"));
     }
 
     @Test
@@ -62,7 +64,7 @@ public class WebTest {
     @Test
     void shouldRejectUncheckedAgreement() {
         fillAndSubmit("Иван Петров", "+79012345678", false);
-        form.$("[data-test-id=agreement].input_invalid").shouldBe(visible);
+        $("[data-test-id=agreement].input_invalid").shouldBe(visible);
         $("[data-test-id=order-success]").shouldNotBe(visible);
     }
 }
